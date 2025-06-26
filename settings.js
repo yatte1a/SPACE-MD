37ee07cb4690e544047ceae829b2867e419e3d0e
Tylor <tylor.9.kid@gmail.com>
2025-06-26 23:30:47 +0300

    Add files via upload

--- settings.js
+++ settings.js
@@ -7,13 +7,15 @@ module.exports = {
   GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
   BOT_PASSWORD: parseInt(process.env.BOT_PASSWORD) || 12345678,
   
+   //Antidelete and Antiedit functionality, set true to enable and false to disable. (⚠️ This function consumes a lot of memory + storage, only enable if you have enough resources ⚠️)
+   ANTI_DELETE: process.env.ANTI_DELETE || 'false',
+  
   
   //⚠️ Premium users settings ⚠️
-  PREMIUM_KEY: process.env.PREMIUM_KEY || '',
-  MAX_SESSIONS: parseInt(process.env.MAX_SESSIONS) || 3,
-  EXPIRY: process.env.EXPIRY || ''
+   PREMIUM_KEY: process.env.PREMIUM_KEY || '',
+   MAX_SESSIONS: parseInt(process.env.MAX_SESSIONS) || 3,
+   EXPIRY: process.env.EXPIRY || ''
 };
 
 // You must set GITHUB_USERNAME to use the bot unless you are a premium user
 // You must fork SPACE-MD repo in order to use the bot unless you're a premium user
-//Need premium? Purchase one here => https://wa.me/tylor
